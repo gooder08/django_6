3eab0f5edc70275456f0d57336ed5e8114e88d57
“gooder08” <gooder08@yandex.ru>
2023-07-11 12:46:43 +0300

    edited

--- logistic/views.py
+++ logistic/views.py
@@ -4,6 +4,7 @@ from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.pagination import LimitOffsetPagination
 
 
 class ProductViewSet(ModelViewSet):
@@ -11,8 +12,9 @@ class ProductViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     # filterset_fields = ['Products',]
-    search_fields = ['title',]
+    search_fields = ['title', 'description']
     ordering_fields = ['id', 'user', 'text', 'created_at']
+    pagination_class = LimitOffsetPagination
     # при необходимости добавьте параметры фильтрации
 
 
